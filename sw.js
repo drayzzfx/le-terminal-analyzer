002d0300be3fd444de36328a694fc7f90f74dcc9
drayzzfx <smk.romain@gmail.com>
2026-06-09 11:50:00 +0200

    feat: PWA + stats avancées (sharpe/expectancy/streak/jour-semaine) + quick outcome + PDF export + calculateur breakeven/simulateur + thème clair + onboarding

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'lt-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './app.html',
+  './calculateur.html',
+  './bubble.html',
+  './menu.js',
+  './logo.jpg.webp'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
